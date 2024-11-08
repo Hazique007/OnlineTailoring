9b37b11f40eec6d03e7ba35d5c8a9b8919e0268f
Online-Tailoring <Theglamthreads21@gmail.com>
2024-11-08 23:16:20 +0530

    Files added

---
+++ client/src/components/AuthHeader.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import image10 from "../assets/images/suit.png";
+
+const AuthHeader = ()=>{
+
+return(
+    <div className="images flex-col justify-between items-center">
+    <img
+        className="h-[80vh] w-full object-cover "
+        src={image10}
+        alt=""
+        srcSet=""
+      />
+
+      </div>
+ 
+    
+)
+
+
+
+
+};
+
+export default AuthHeader;
