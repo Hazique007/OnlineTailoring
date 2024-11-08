194f172ac8c151d195df45b6b5157d4bee2493ea
Samar Abbas <samarabbas172003@gmail.com>
2024-11-08 19:58:31 +0530

    Add Skip Button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/Landing-Page/landing";
 import ProductPage from "./pages/Product-Page/product";
 import SearchPage from "./pages/Search-Page/SearchPage";
+import LoginPage from "./pages/Authentication/Login/login";
+import AuthLanding from "./pages/Authentication/AuthLanding";
+
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" Component={LandingPage}></Route>
+          <Route path="/" Component={AuthLanding}></Route>
+          <Route path="/login" Component={LoginPage}></Route>
+          <Route path="/Home" Component={LandingPage}></Route>
           <Route path="/product" Component={ProductPage}></Route>
           <Route path="/search" Component={SearchPage}></Route>
         </Routes>
--- client/src/components/Headerphoto.jsx
+++ client/src/components/Headerphoto.jsx
@@ -1,32 +1,27 @@
 import React from "react";
 
 import image10 from "../assets/images/headerphoto.png";
+import { useNavigate } from "react-router-dom";
 
+const Headerphoto = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="images flex-col justify-between items-center ">
+      <button
+        className="absolute top-3 right-3 text-white font-poppins font-medium px-4 py-1 bg-slate-900 rounded-md shadow-md hover:bg-blue-700 transition-all duration-200 ease-in-out"
+        onClick={() => navigate("/Home")}
+      >
+        Skip
+      </button>
 
-const Headerphoto = ()=> {
-
-    return(
-        <div className="images flex-col justify-between items-center">
-        <img
-            className="h-[30vh] w-full object-cover "
-            src={image10}
-            alt=""
-            srcSet=""
-          />
-
-          </div>
-     
-
-
-
-
-    )
-
-
-
-
-
-
-}
+      <img
+        className="h-[30vh] w-full object-cover "
+        src={image10}
+        alt=""
+        srcSet=""
+      />
+    </div>
+  );
+};
 
 export default Headerphoto;
