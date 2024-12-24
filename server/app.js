34415d047adc14e9bb75358460124039e64f2d49
Samar Abbas <samarabbas172003@gmail.com>
2024-12-25 03:50:55 +0530

    Bac

--- server/app.js
+++ server/app.js
@@ -29,10 +29,11 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-app.use(express.static(path.join(__dirname, "../client/dist")));
+// app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+app.get("/", (req, res) => {
+  // res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+  res.status(200).send("Open it again");
 });
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
