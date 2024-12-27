e3e4e303224a14bfa3909154bf44ea7f92de4598
Samar Abbas <samarabbas172003@gmail.com>
2024-12-27 21:34:18 +0530

    12px

--- client/src/components/Trending.jsx
+++ client/src/components/Trending.jsx
@@ -57,7 +57,7 @@ const Trending = () => {
 
   return (
     <div className="px-4 mt-10 h-auto rounded-lg">
-      <h1 className="font-poppins ml-1 font-bold text-[14px] leading-[18px]">
+      <h1 className="font-poppins ml-1 font-bold text-[12px] leading-[18px]">
         Trending
       </h1>
 
