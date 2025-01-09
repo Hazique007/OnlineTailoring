8c2ba644d229b3b51da6868f518c9ad49fcba9f7
Samar Abbas <samarabbas172003@gmail.com>
2025-01-10 00:35:40 +0530

    r

--- server/app.js
+++ server/app.js
@@ -21,17 +21,30 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __dirname = path.dirname(__filename);
 
 // Middleware
 
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ limit: "500mb", extended: true }));
-app.use(cors({ credentials: true, methods: ["GET", "POST", "PUT", "DELETE"] }));
+const isProduction = process.env.NODE_ENV === "production";
+
+app.use(
+  cors({
+    origin: isProduction
+      ? "https://onlinetailoring.apnadarzi.in"
+      : "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 app.use(bodyParser.json());
 
 database();
-
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 // API Routes
 app.use("/uploads", express.static("uploads"));
 
