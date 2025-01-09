e04ff06c9042ecddec13dc989f28043a2f036930
Samar Abbas <samarabbas172003@gmail.com>
2025-01-09 21:06:37 +0530

    Add redis Cachin4

--- server/server.js
+++ server/server.js
@@ -1,25 +1,3 @@
-
-// const dotenv =require("dotenv");
-// const Address = require("./models/addresschema")
-// dotenv.config();
-// const cors = require("cors");
-// app.use(cors());
-// const addressRoute = require("./routes/AddressRoute");
-// const personalDetailsRoute = require("./routes/PersonalDetailsRoutes")
-// const UserRoute = require("./routes/UserRoute")
-// // Increase the limit to 10mb for JSON data
-// app.use(express.json({ limit: "10mb" }));
-// // For URL-encoded data
-// app.use(express.urlencoded({ limit: "10mb", extended: true }));
-
-
-
-
-
-
-
-
-
 // mongoose.connect(process.env.URI)
 // .then(()=>{
 //     console.log("connected successfully");
@@ -27,18 +5,14 @@
 //         if(err) console.log(err);
 
 //         console.log("running successfully at",process.env.PORT);
-        
-        
+
 //     });
-    
+
 // }).catch((err)=>{
 // console.log("error:",err);
 
 // })
 
-
 // app.use(addressRoute);
 // app.use(personalDetailsRoute);
 // app.use('/api',UserRoute)
-
-
