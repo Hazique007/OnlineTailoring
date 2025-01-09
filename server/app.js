a7203584902fbab9f3a3f299161697a8b0ebd22c
Samar Abbas <samarabbas172003@gmail.com>
2025-01-10 00:52:10 +0530

    p

--- server/app.js
+++ server/app.js
@@ -27,13 +27,10 @@ const __filename = fileURLToPath(import.meta.url);
 
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ limit: "500mb", extended: true }));
-const isProduction = process.env.NODE_ENV === "production";
+// const isProduction = process.env.NODE_ENV === "production";
 
 app.use(
   cors({
-    origin: isProduction
-      ? "https://onlinetailoring.apnadarzi.in"
-      : "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
